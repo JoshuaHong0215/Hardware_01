1afd52cd8faf72b9753004d6007953a9b28b196e
JoshuaHong0215 <sebin5736@gmail.com>
2025-07-23 14:39:16 +0900

    아두이노 코드 수정

--- HC-04SR/HC-04SR.ino
+++ HC-04SR/HC-04SR.ino
@@ -1,15 +1,8 @@
-/*
- * HC-SR04 example sketch
- *
- * https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380
- *
- * by Isaac100
- */
-
+// 전역 변수
 const int trigPin = 9;
 const int echoPin = 10;
 
-float duration, distance;
+float duration;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -17,16 +10,25 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+float getFilteredDistance(int times = 5) {
+  float sum = 0;
+  for (int i = 0; i < times; i++) {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
-//  Serial.print("Distance: ");
+    duration = pulseIn(echoPin, HIGH, 30000); // 30ms timeout
+    float d = (duration * 0.0343) / 2;
+    sum += d;
+    delay(50);  // 센서 간섭 방지
+  }
+  return sum / times;
+}
+
+void loop() {
+  float distance = getFilteredDistance(5);
   Serial.println(distance);
   delay(100);
 }
