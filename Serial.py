4ed43c816ac12cb85aed414dc4a7503cd74872e4
JoshuaHong0215 <sebin5736@gmail.com>
2025-07-23 11:09:01 +0900

    Serial.py_코드 내용 수정_01
    코드 내용 수정 ver_01

--- Serial.py
+++ Serial.py
@@ -1,25 +1,23 @@
 import serial
 import time
 
-#전역변수
+# 전역 변수
 connection = None
 current_distance = 0
 
-def connect_sencor(port='COM4'):
+def connect_sensor(port='COM4'):
     global connection
     try:
-        connection = serial.Serial(port,9600)
+        connection = serial.Serial(port, 9600)
         time.sleep(2)
-        print("연결성공")
+        print("연결 성공")
         return True
-    
     except:
-        print("연결실패")
+        print("연결 실패")
         return False
-    
-    
+
 def read_distance():
-    global connection, currnet_distance
+    global connection, current_distance
     if connection and connection.in_waiting > 0:
         data = connection.readline().decode().strip()
         try:
@@ -28,16 +26,15 @@ def read_distance():
             return distance
         except:
             pass
-        return None
-    
-    
+    return None
+
 def main():
     if connect_sensor():
         for i in range(10):
             dist = read_distance()
             if dist:
-                print(f'거리 : {dist}cm')
+                print(f"거리: {dist}cm")
             time.sleep(0.5)
-            
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
